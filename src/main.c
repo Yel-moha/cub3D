f5a929124520038b036a7c9b023487eb692b2cbd
Youssef El Mohajir <yel-moha@c1r3p6.42firenze.it>
2026-04-07 16:13:53 +0200

    aggiungo file.c per iniziare a spezzare il main troppo lungo

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yel-moha <yel-moha@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 15:32:37 by yel-moha          #+#    #+#             */
-/*   Updated: 2026/04/04 11:46:32 by yel-moha         ###   ########.fr       */
+/*   Updated: 2026/04/07 16:13:42 by yel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,30 +34,15 @@ void	free_split(char **split)
 
 int main(int argc, char **argv)
 {
-	int		fd;
-	char	*line;
-	char	**split;
-	const char	*map_path;
+	
 	t_tex_paths *pos_text;
+	const char	*map_path;
 
-	pos_text = ft_calloc(1, sizeof(t_tex_paths));
+	
 	map_path = "src/valid_minimal.cub";
 	if (argc == 2)
 		map_path = argv[1];
-	fd = open(map_path, O_RDONLY);
-	if (fd < 0)
-	{
-		perror("open");
-		return (1);
-	}
-	line = get_next_line(fd);
-	if (!line)
-	{
-		printf("file vuoto o non leggibile: %s\n", map_path);
-		close(fd);
-		free(pos_text);
-		return (1);
-	}
+
 	while (line)
 	{		
 		split = ft_split(line, ' ');
--- src/parse_line.c
+++ src/parse_line.c
@@ -6,7 +6,7 @@
 /*   By: yel-moha <yel-moha@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 15:48:58 by yel-moha          #+#    #+#             */
-/*   Updated: 2026/04/07 16:03:55 by yel-moha         ###   ########.fr       */
+/*   Updated: 2026/04/07 16:13:30 by yel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 char **parse_textures(char *line, t_tex_paths *pos)
 {
+    int         fd;
     char        **split;
     char        *line;
-    t_tex_paths *pos;
+    t_tex_paths *pos_text;
 
+    pos_text = ft_calloc(1, sizeof(t_tex_paths));
+    fd = open(map_path, O_RDONLY);
+    line = get_next_line(fd);
+	if (!line)
+	{
+		printf("file vuoto o non leggibile: %s\n", map_path);
+		free(pos_text);
+		return (1);
+	}
     while(line)
     {
         split = ft_split(line, ' ');
@@ -32,9 +42,20 @@ void split_errors(char **split)
     }
 }
 
-void  line_errors(char *line)
+void  line_errors(char *line, int fd)
 {
-    
+    if (fd < 0)
+	{
+		perror("open");
+		return (1);
+	}
+	if (!line)
+	{
+		printf("file vuoto o non leggibile: %s\n", map_path);
+		close(fd);
+		free(pos_text);
+		return (1);
+	}
 }
 char **parse_colors(char **line)
 {
